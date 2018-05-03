b4b866c15fdb6258e0e17a09d376b7ffb04a293c
takahashim <maki@rubycolor.org>
2018-05-03 21:57:52 +0900

    support footnote

--- test/markdown_test.rb
+++ test/markdown_test.rb
@@ -28,10 +28,8 @@ EOB
     md = Cheepub::Markdown.new("この問題は{今度|こんど}の{試験|テスト}に出る。\n")
     expected = "この問題は<ruby><rb>今度</rb><rp>（</rp><rt>こんど</rt><rp>）</rp></ruby>の<ruby><rb>試験</rb><rp>（</rp><rt>テスト</rt><rp>）</rp></ruby>に出る。"
     result = md.convert
-    p result
     result =~ /<p>(.*)<\/p>/
     para = $1
-    p para
     assert_equal(expected, para)
   end
 
@@ -40,10 +38,27 @@ EOB
     md = Cheepub::Markdown.new("平成^30^年^!?^\n")
     expected = %q{平成<span class="tcy">30</span>年<span class="tcy">!?</span>}
     result = md.convert
-    p result
     result =~ /<p>(.*)<\/p>/
     para = $1
-    p para
+    assert_equal(expected, para)
+  end
+
+  def test_convert_footnote
+    md = Cheepub::Markdown.new("テスト[^1]です。\n\n[^1]: テストの脚注です。\n")
+    expected = <<-EOB
+    <p>テスト<sup id="fnref:1"><a href="#fn:1" class="footnote">1</a></sup>です。</p>
+
+<div class="footnotes">
+  <ol>
+    <li id="fn:1">
+      <p>テストの脚注です。 <a href="#fnref:1" class="reversefootnote">&#8617;</a></p>
+    </li>
+  </ol>
+</div>
+EOB
+    result = md.convert
+    result =~ /<body[^>]+>\n(.*)  +<\/body>/m
+    para = $1
     assert_equal(expected, para)
   end
 end
