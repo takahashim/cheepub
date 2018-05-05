bc9c7dd735e13ad5fa208b495a3b43428aaf62c7
takahashim <maki@rubycolor.org>
2018-05-06 01:17:21 +0900

    Hash#symbolize_keys! should be recursive

--- lib/cheepub/ext_hash.rb
+++ lib/cheepub/ext_hash.rb
@@ -3,6 +3,9 @@ module Cheepub
     refine Hash do
       def symbolize_keys!
         keys.each do |key|
+          if self[key].kind_of? Hash
+            self[key].symbolize_keys!
+          end
           self[(key.to_sym rescue nil) || key] = delete(key)
         end
         self
