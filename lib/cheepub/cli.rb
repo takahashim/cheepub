17aa3ea0bcea9d6329c274b9f8590e99ea4a5cb1
takahashim <maki@rubycolor.org>
2018-05-06 01:57:34 +0900

    add options --author, --title, --config

--- lib/cheepub/cli.rb
+++ lib/cheepub/cli.rb
@@ -1,14 +1,25 @@
 module Cheepub
   class CLI < Clamp::Command
+    using Cheepub::ExtHash
+
     option ["-v", "--version"], :flag, "Show version" do
       puts Cheepub::VERSION
       exit(0)
     end
+    option ["--author"], "AUTOR", "author of the book"
+    option ["--title"],  "TITLE", "title of the book"
+    option ["--config"],  "CONFIG", "configuration file"
 
     parameter "SRC", "source file"
 
     def execute
-      gen = Cheepub::Generator.new(src)
+      params = {}
+      if config
+        params = YAML.safe_load(config).symbolize_keys!
+      end
+      params[:author] = author if author
+      params[:title] = title if title
+      gen = Cheepub::Generator.new(src, params)
       begin
         gen.execute
       rescue Cheepub::Error => e
