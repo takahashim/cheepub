845ef99e4d9cf47611b16bda70ee0f695a0ca871
takahashim <maki@rubycolor.org>
2018-05-10 12:31:04 +0900

    LaTeX: remove listings, need more language support... (tons of errors occured)

--- lib/cheepub/converter/cheelatex.rb
+++ lib/cheepub/converter/cheelatex.rb
@@ -3,6 +3,29 @@ require "kramdown/converter/latex"
 module Cheepub
   module Converter
     module CheeLatex
+
+      def convert_codeblock(el, opts)
+        show_whitespace = el.attr['class'].to_s =~ /\bshow-whitespaces\b/
+        lang = extract_code_language(el.attr)
+
+        if @options[:syntax_highlighter] == :minted &&
+            (highlighted_code = highlight_code(el.value, lang, :block))
+          @data[:packages] << 'minted'
+          "#{latex_link_target(el)}#{highlighted_code}\n"
+        elsif (show_whitespace || lang) && @options[:syntax_highlighter]
+          options = []
+          options << "showspaces=%s,showtabs=%s" % (show_whitespace ? ['true', 'true'] : ['false', 'false'])
+          options << "language=#{lang}" if lang
+          options << "basicstyle=\\ttfamily\\footnotesize,columns=fixed,frame=tlbr"
+          id = el.attr['id']
+          options << "label=#{id}" if id
+          attrs = attribute_list(el)
+          "#{latex_link_target(el)}\\begin{lstlisting}[#{options.join(',')}]\n#{el.value}\n\\end{lstlisting}#{attrs}\n"
+        else
+          "#{latex_link_target(el)}\\begin{verbatim}#{el.value}\\end{verbatim}\n"
+        end
+      end
+
       def convert_html_element(el, opts)
         if el.value == :ruby
           str = inner(el, opts)
