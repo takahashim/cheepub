b74a3e09fef9413eaf56beaa01bacc8a530661bc
takahashim <maki@rubycolor.org>
2018-05-20 23:50:29 +0900

    use Pathname for `relative_path_from`

--- lib/cheepub/asset_store.rb
+++ lib/cheepub/asset_store.rb
@@ -2,27 +2,29 @@ require 'digest/sha2'
 require 'base64'
 require 'fileutils'
 require 'tmpdir'
+require 'pathname'
 
 module Cheepub
   class AssetStore
     def initialize(asset_dir)
       @store = {}
-      @asset_dir = asset_dir
+      @asset_dir = Pathname.new(asset_dir)
     end
 
-    def store(src)
+    def store(src, dir)
       ext = File.extname(src)
       img_content = open(src){|f| f.read}
-      img_path = asset_filename(img_content, ext)
+      img_path = asset_file_path(img_content, dir, ext)
       if !@store[src]
         @store[src] = img_path
-        File.write(img_path, img_content)
+        FileUtils.mkdir_p(img_path.dirname.to_s)
+        File.write(img_path.to_s, img_content)
       end
-      img_path
+      img_path.relative_path_from(@asset_dir)
     end
 
     def clear
-      FileUtils.remove_entry_secure(@asset_dir)
+      FileUtils.remove_entry_secure(@asset_dir.to_s)
     end
 
     def [](key)
@@ -33,10 +35,11 @@ module Cheepub
       @store[key] = val
     end
 
-    def asset_filename(content, ext)
+    def asset_file_path(content, dir, ext)
       hash_val = Digest::SHA256.digest(content)
       basename = Base64.urlsafe_encode64(hash_val)
-      File.join(@asset_dir, "#{basename}#{ext}")
+      path = @asset_dir + dir + "#{basename}#{ext}"
+      path
     end
   end
 end
